936ec554669daf7992aec1425dec2a405d30b563
jinshen <kim.jin@ajtech-software.com>
2014-03-06 14:33:16 +0800

    PackageOperationView.xaml has been added to solution.
    Signed-off-by: jinshen <kim.jin@ajtech-software.com>

--- AJ.DMES.PackageWorkstation.UI.Wpf/ViewModel/HomeViewModel.cs
+++ AJ.DMES.PackageWorkstation.UI.Wpf/ViewModel/HomeViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 
 /***************************************************************************
@@ -19,5 +20,65 @@ namespace AJ.DMES.PackageWorkstation.UI.Wpf.ViewModel
 {
     public class HomeViewModel : ViewModelBase
     {
+        public ICommand NavigateToPackageCommand { get; private set; }
+        public ICommand NavigateToTraceabilityCommand { get; private set; }
+        public ICommand NaviagteToModelCommand { get; private set; }
+        public ICommand NavigateToWICommand { get; private set; }
+        public ICommand NavigateToCustomerCommand { get; private set; }
+        public ICommand NavigateToStatisticCommand { get; private set; }
+        public ICommand NavigateToConfigCommand { get; private set; }
+        public ICommand NavigateToUserCommand { get; private set; }
+
+        public HomeViewModel()
+        {
+            NavigateToPackageCommand = new DelegateCommand(OnNavigateToPackageCommandExecute);
+            NavigateToTraceabilityCommand = new DelegateCommand(OnNavigateToTraceabilityCommandExecute);
+            NaviagteToModelCommand = new DelegateCommand(OnNavigateToModelCommandExecute);
+            NavigateToWICommand = new DelegateCommand(OnNavigateToWiCommandExecute);
+            NavigateToCustomerCommand = new DelegateCommand(OnNavigateToCustomerCommandExecute);
+            NavigateToStatisticCommand = new DelegateCommand(OnNavigateToStatisticCommandExecute);
+            NavigateToConfigCommand = new DelegateCommand(OnNavigateToConfigCommandExecute);
+            NavigateToUserCommand = new DelegateCommand(OnNavigateToUserCommandExecute);
+        }
+
+        void OnNavigateToPackageCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("PackageOperationView", null, this);
+        }
+
+        void OnNavigateToTraceabilityCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("TraceabilityView", null, this);
+        }
+
+        void OnNavigateToModelCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("ModelView", null, this);
+        }
+
+        void OnNavigateToWiCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("WorkInstructionView", null, this);
+        }
+
+        void OnNavigateToCustomerCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("CustomerView", null, this);
+        }
+
+        void OnNavigateToStatisticCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("StatisticView", null, this);
+        }
+
+        void OnNavigateToConfigCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("ConfigurationView", null, this);
+        }
+
+        void OnNavigateToUserCommandExecute()
+        {
+            ServiceContainer.GetService<INavigationService>().Navigate("UserManagementView", null, this);
+        }
     }
 }
