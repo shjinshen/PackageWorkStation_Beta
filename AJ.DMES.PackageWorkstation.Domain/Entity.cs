e4668d77470c1744b263bc1b4269eb7e0438b7bd
jinshen <kim.jin@ajtech-software.com>
2014-03-06 15:23:25 +0800

    Add Requirement
    PackageWorkstation_Requirement.doc

---
+++ AJ.DMES.PackageWorkstation.Domain/Entity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/***************************************************************************
+*Class Name: Entity
+*Description:
+*Author: Jinshen
+*Creation Time: 2014/3/6 15:09:57
+*Modified by:
+*Modification Time:
+*@Version 1.0
+****************************************************************************/
+
+namespace AJ.DMES.PackageWorkstation.Domain
+{
+    public abstract class Entity<TId>
+    {
+        public virtual TId Id { get; protected set; }
+        public virtual int Version { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity<TId>);
+        }
+
+        public virtual bool Equals(Entity<TId> other)
+        {
+            if (other == null)
+                return false;
+            if (ReferenceEquals(this, other))
+                return true;
+            if (!IsTransient(this)
+               && !IsTransient(other)
+               && Equals(Id, other.Id))
+            {
+                var otherType = other.GetUnProxiedType();
+                var thisType = GetUnProxiedType();
+                return thisType.IsAssignableFrom(otherType) ||
+                    otherType.IsAssignableFrom(thisType);
+            }
+            return false;
+            
+        }
+
+        public override int GetHashCode()
+        {
+            if (Equals(Id, default(TId)))
+                return base.GetHashCode();
+            return Id.GetHashCode();
+        }
+
+        private static bool IsTransient(Entity<TId> obj)
+        {
+            return obj != null && Equals(obj.Id, default(TId));
+        }
+
+        private Type GetUnProxiedType()
+        {
+            return GetType();
+        }
+    }
+
+    public abstract class Entity : Entity<Guid>
+    {
+    }
+}
---
+++ AJ.DMES.PackageWorkstation.Domain/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/***************************************************************************
+*Class Name: Model
+*Description:
+*Author: Jinshen
+*Creation Time: 2014/3/6 15:15:47
+*Modified by:
+*Modification Time:
+*@Version 1.0
+****************************************************************************/
+
+namespace AJ.DMES.PackageWorkstation.Domain
+{
+    public class Model : Entity
+    {
+        public string ModelID { get; set; }
+        public string Description { get; set; }
+    }
+}
