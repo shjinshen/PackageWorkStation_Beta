678ca9d2460fc0c6f5c45875edcf3557af4f19a9
jinshen <kim.jin@ajtech-software.com>
2014-03-10 09:45:42 +0800

    ModelView.xaml and CustomerView.xaml have been added to the project.
    Signed-off-by: jinshen <kim.jin@ajtech-software.com>

---
+++ AJ.DMES.PackageWorkstation.UI.Wpf/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Xpf.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/***************************************************************************
+*Class Name: CustomerViewModel
+*Description:
+*Author: Jinshen
+*Creation Time: 2014/3/10 9:18:25
+*Modified by:
+*Modification Time:
+*@Version 1.0
+****************************************************************************/
+
+namespace AJ.DMES.PackageWorkstation.UI.Wpf.ViewModel
+{
+    public class CustomerViewModel : ViewModelBase
+    {
+    }
+}
---
+++ AJ.DMES.PackageWorkstation.UI.Wpf/ViewModel/ModelViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.Xpf.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/***************************************************************************
+*Class Name: ModelViewModel
+*Description:
+*Author: Jinshen
+*Creation Time: 2014/3/10 9:40:55
+*Modified by:
+*Modification Time:
+*@Version 1.0
+****************************************************************************/
+
+namespace AJ.DMES.PackageWorkstation.UI.Wpf.ViewModel
+{
+    public class ModelViewModel : ViewModelBase
+    {
+    }
+}
