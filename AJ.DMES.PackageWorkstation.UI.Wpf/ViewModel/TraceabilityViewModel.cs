369a217a664fa40dbfc31718a5c66b0ca705ce9a
jinshen <kim.jin@ajtech-software.com>
2014-03-10 14:19:50 +0800

    Add doc

---
+++ AJ.DMES.PackageWorkstation.UI.Wpf/ViewModel/TraceabilityViewModel.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Xpf.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/***************************************************************************
+*Class Name: TraceabilityViewModel
+*Description:
+*Author: Jinshen
+*Creation Time: 2014/3/10 13:56:12
+*Modified by:
+*Modification Time:
+*@Version 1.0
+****************************************************************************/
+
+namespace AJ.DMES.PackageWorkstation.UI.Wpf.ViewModel
+{
+    public class TraceabilityViewModel : ViewModelBase
+    {
+
+    }
+}
